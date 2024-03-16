ed0b4282c4d23eff4906d874ace6c57be3488055
mafik22 <Shelengmark765@gmail.com>
2024-03-16 19:53:00 +0200

    hello

--- dz_6.py
+++ dz_6.py
@@ -11,12 +11,12 @@ def divider(a, b):
         if b > 100:
             raise IndexError("b має бути менше або рівне 100")
         return a / b
-    except ValueError as ve:
-        print("ValueError:", ve)
-    except ZeroDivisionError as zde:
-        print("ZeroDivisionError:", zde)
-    except IndexError as ie:
-        print("IndexError:", ie)
+    except ValueError:
+        print("ValueError:")
+    except ZeroDivisionError:
+        print("ZeroDivisionError:")
+    except IndexError:
+        print("IndexError:")
 
 data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
 for key in data:
